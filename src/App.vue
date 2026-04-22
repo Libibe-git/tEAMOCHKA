a271547c07a4a29323307937cdeb5c89018b3f64
Havi <ps-working@yandex.ru>
2026-04-22 18:12:30 +0300

    Сделаны роутеры

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,33 @@
 </script>
 
 <template>
-<div class="gg"><h1>GG</h1></div>
+    <div class="gg">
+        <h1>GG</h1>
+    </div>
+    <div class="flex">
+        <RouterLink :to="{ name: 'page1' }">Первая страница</RouterLink>
+        <RouterLink :to="{ name: 'page2' }">Вторая страница</RouterLink>
+        <RouterLink :to="{ name: 'page3' }">Третья страница</RouterLink>
+
+    </div>
+
+    <RouterView></RouterView>
 </template>
 
-<style scoped></style>
+<style scoped>
+.flex{
+    display: flex;
+    justify-content: center;
+    gap: 20px;
+}
+
+a{
+    color: black;
+    font-size: 20px;
+}
+
+a:hover{
+    color:cornflowerblue;
+}
+
+</style>
