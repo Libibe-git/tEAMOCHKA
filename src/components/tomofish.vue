aad832d624ee5d72215f10742553a4493aa0a7c4
Havi <ps-working@yandex.ru>
2026-04-27 09:37:25 +0300

    исправлен баг, рыбка больше не выплывает за пределы аквариума

--- src/components/tomofish.vue
+++ src/components/tomofish.vue
@@ -3,28 +3,49 @@ import { ref } from 'vue';
 
 const fish_x = ref(400);
 const fish_y = ref(200);
+const random_x = ref(0)
+const random_y = ref(0)
 
 function GoFish() {
-    fish_x.value += Math.random()* 200 - 150
-    fish_y.value += Math.random()* 200 - 150
+    random_x.value = Math.random() * 200 - 150
+    random_y.value = Math.random() * 200 - 150
 
+    if ((fish_x.value + random_x.value) <= 900 && (fish_x.value + random_x.value) >= 0) {
+        fish_x.value += random_x.value
+    }else{
+        fish_x.value = 400
+    }
+
+    if ((fish_y.value + random_y.value) <= 1000 && (fish_y.value + random_y.value) >= 0) {
+        fish_y.value += random_y.value
+    }else{
+        fish_y.value = 600
+    }
+
+    console.log(fish_y.value + random_y.value);
+    console.log(fish_x.value + random_x.value);
+    
 }
 
-setInterval(GoFish, 5000);
+setInterval(GoFish, 2000);
 
 </script>
 
 <template>
     <div>
-        <h1>Тут будет рыбка</h1>
+        <h1>Томогочи-fish</h1>
     </div>
     <div class="acvar">
         <div class="fish"> <img src="../assets/fish.png" alt=""></div>
 
     </div>
     <div class="flex">
-        <label for=""><div class="btn"><img src="../assets/eat.jpg" alt=""></div><strong>Покормить</strong></label>
-        <label for=""><div class="btn"><img src="../assets/filter.webp" alt=""></div><strong>Почистить фильтр</strong></label>
+        <label for="">
+            <div class="btn"><img src="../assets/eat.jpg" alt=""></div><strong>Покормить</strong>
+        </label>
+        <label for="">
+            <div class="btn"><img src="../assets/filter.webp" alt=""></div><strong>Почистить фильтр</strong>
+        </label>
     </div>
 </template>
 
@@ -47,7 +68,7 @@ setInterval(GoFish, 5000);
     background: rgb(255, 255, 255);
     border-radius: 100%;
     overflow: hidden;
-    transition: 1s;
+    transition: 1.6s;
 }
 
 .fish img {
