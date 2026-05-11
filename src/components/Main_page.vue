c0e820e74da9928667e44ab1e6685bd1bed8de31
kikikith <kira.chestnova@mail.ru>
2026-05-12 01:28:23 +0300

    добавлены роутеры

--- src/components/Main_page.vue
+++ src/components/Main_page.vue
@@ -1,174 +1,177 @@
 <script setup>
+import { ref } from "vue";
+import { useRoute } from "vue-router";
 
-import { ref } from 'vue'
-import { useRoute } from 'vue-router'
-
-const styles = ref('#dd8c8c')
-let index = 0
+const styles = ref("#dd8c8c");
+let index = 0;
 
 function rColor() {
-    if (route.name == 'Main_page') {
-        let spisok = ref(['orange', 'blue', 'green', 'grey'])
-        styles.value = spisok.value[index]
+  if (route.name == "Main_page") {
+    let spisok = ref(["orange", "blue", "green", "grey"]);
+    styles.value = spisok.value[index];
+  } else {
+    styles.value = "black";
+  }
 
-    } else {
-        styles.value = 'black'
-
-    }
-
-    // console.log(spisok.value[index]);
-    if (index < 2) {
-        index = index + 1
-
-    } else {
-        index = 0
-    }
-
+  // console.log(spisok.value[index]);
+  if (index < 2) {
+    index = index + 1;
+  } else {
+    index = 0;
+  }
 }
 
-setInterval(rColor, 1000)
+setInterval(rColor, 1000);
 
-
-const route = useRoute()
+const route = useRoute();
 </script>
 <template>
-
-    <div class="welcome">
-        <div>
-            <h1>Добро пожаловать, приятной игры</h1>
-        </div>
-
-        <div class="text_contain">
-            <p class="text">«Сделай паузу! Наш сайт — это место, где можно забыть о делах и просто насладиться
-                игрой.
-                Выбирай своё приключение на сегодня!»</p>
-        </div>
+  <div class="welcome">
+    <div>
+      <h1>Добро пожаловать, приятной игры</h1>
     </div>
 
-    <div class="flex">
-        <RouterLink :to="{ name: 'tomofish' }">
-            <div class="card">
-                <div class="img_fish">
-                </div>
-                <div class="opisanie-t">Томогочи-fish</div>
-            </div>
-        </RouterLink>
-        <RouterLink :to="{ name: 'pin_pong' }">
-            <div class="card">
-                <div class="img_ping">
-                </div>
-                <div class="opisanie-p">Пин-Понг</div>
-            </div>
-        </RouterLink>
-        <RouterLink :to="{ name: 'football' }">
-            <div class="card">
-                <div class="img_football"></div>
-                <div class="opisanie-f">Футик</div>
-            </div>
-
-        </RouterLink>
+    <div class="text_contain">
+      <p class="text">
+        «Сделай паузу! Наш сайт — это место, где можно забыть о делах и просто
+        насладиться игрой. Выбирай своё приключение на сегодня!»
+      </p>
     </div>
+  </div>
+
+  <div class="flex">
+    <RouterLink :to="{ name: 'tomofish' }">
+      <div class="card">
+        <div class="img_fish"></div>
+        <div class="opisanie-t">Томогочи-fish</div>
+      </div>
+    </RouterLink>
+    <RouterLink :to="{ name: 'pin_pong' }">
+      <div class="card">
+        <div class="img_ping"></div>
+        <div class="opisanie-p">Пин-Понг</div>
+      </div>
+    </RouterLink>
+    <RouterLink :to="{ name: 'football' }">
+      <div class="card">
+        <div class="img_football"></div>
+        <div class="opisanie-f">Футик</div>
+      </div>
+    </RouterLink>
+        <RouterLink :to="{ name: 'game' }">
+            <div class="card">
+                <div class="img_catch"></div>
+                <div class="opisanie-c">Попробуй поймать</div>
+            </div>
+        </RouterLink>
+  </div>
 </template>
 
 <style scoped>
 .flex {
-    display: flex;
-    justify-content: center;
-    gap: 20px;
-    font-family: fantasy;
-    min-width: 100vw;
-    /* margin-bottom: 700px; */
-    /* flex: 1 0 auto; */
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+  font-family: fantasy;
+  min-width: 100vw;
+  /* margin-bottom: 700px; */
+  /* flex: 1 0 auto; */
 }
 
 .card {
-    width: 385px;
-    min-height: 350px;
-    border: 1px solid grey;
-    border-radius: 10%;
-    padding: 0;
-    overflow: hidden;
-    box-shadow: 2px 2px 10px black;
-    font-size: 28px;
-    background: rgba(240, 248, 255, 0.781);
+  width: 385px;
+  min-height: 350px;
+  border: 1px solid grey;
+  border-radius: 10%;
+  padding: 0;
+  overflow: hidden;
+  box-shadow: 2px 2px 10px black;
+  font-size: 28px;
+  background: rgba(240, 248, 255, 0.781);
 }
 
 .card:hover {
-    width: 520px;
-    /* transform: translate(-10px, -10px); */
-    transition: 1.6s;
-    box-shadow: 0 20px 20px rgb(104, 104, 104);
+  width: 520px;
+  /* transform: translate(-10px, -10px); */
+  transition: 1.6s;
+  box-shadow: 0 20px 20px rgb(104, 104, 104);
 }
 
 .img_fish {
-    background-image: url(../assets/rich_fish.jpg);
-    background-size: cover;
-    background-repeat: no-repeat;
-    width: 100%;
-    height: 250px;
+  background-image: url(../assets/rich_fish.jpg);
+  background-size: cover;
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 250px;
 }
 
 .img_ping {
-    background-image: url(../assets/luna.jpg);
-    background-size: cover;
-    background-repeat: no-repeat;
-    width: 100%;
-    height: 250px;
-    background-position: 50%
+  background-image: url(../assets/luna.jpg);
+  background-size: cover;
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 250px;
+  background-position: 50%;
 }
 
 .img_football {
-    background-image: url(../assets/img/pole.jpg);
-    background-size: cover;
-    background-repeat: no-repeat;
-    width: 100%;
-    height: 250px;
-    background-position: 100%
+  background-image: url(../assets/img/pole.jpg);
+  background-size: cover;
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 250px;
+  background-position: 100%;
+}
+.img_catch {
+  background-image: url(../assets/img_catch.png);
+  background-size: cover;
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 250px;
+  background-position: 100%;
 }
 
 .opisanie-t {
-    padding-top: 10px;
-    -webkit-text-stroke: 1px rgb(253, 238, 151);
-    color: rgb(255, 123, 0);
-    letter-spacing: 2px;
+  padding-top: 10px;
+  -webkit-text-stroke: 1px rgb(253, 238, 151);
+  color: rgb(255, 123, 0);
+  letter-spacing: 2px;
 }
 
 .opisanie-p {
-    padding-top: 10px;
-    -webkit-text-stroke: 1px rgb(114, 176, 247);
-    color: rgb(0, 48, 138);
-    letter-spacing: 2px;
+  padding-top: 10px;
+  -webkit-text-stroke: 1px rgb(114, 176, 247);
+  color: rgb(0, 48, 138);
+  letter-spacing: 2px;
 }
 
-.opisanie-f {
-    padding-top: 10px;
-    -webkit-text-stroke: 1px rgb(69, 179, 5);
-    color: rgb(55, 69, 0);
-    letter-spacing: 2px;
+.opisanie-c {
+  padding-top: 10px;
+  -webkit-text-stroke: 1px rgb(90, 3, 81);
+  color: rgb(202, 5, 205);
+  letter-spacing: 2px;
 }
 
 .welcome {
-    min-width: 100vw;
+  min-width: 100vw;
 }
 
 .welcome h1 {
-    color: v-bind(styles);
-    font-family: fantasy;
-    letter-spacing: 2px;
-    -webkit-text-stroke: 1px rgb(255, 255, 255);
-
-
+  color: v-bind(styles);
+  font-family: fantasy;
+  letter-spacing: 2px;
+  -webkit-text-stroke: 1px rgb(255, 255, 255);
 }
 
 .text {
-    font-size: 36px;
-    font-family: fantasy;
-    letter-spacing: 1.5px;
-    padding: 2rem;
-    -webkit-text-stroke: 0.5px black;
+  font-size: 36px;
+  font-family: fantasy;
+  letter-spacing: 1.5px;
+  padding: 2rem;
+  -webkit-text-stroke: 0.5px black;
 }
 
 .text_contain {
-    margin-bottom: 40px;
+  margin-bottom: 40px;
 }
 </style>
--- src/route.js
+++ src/route.js
@@ -3,13 +3,16 @@ import Main_page from './components/Main_page.vue'
 import tomofish from './components/tomofish.vue'
 import pin_pong from './components/pin-pong.vue'
 import football from './components/football.vue'
+import CatchGame from './components/CatchGame.vue'
 
 
 const routes = [
     {path: '/', component: Main_page, name: 'Main_page'},
     {path: '/tomofish', component: tomofish, name: 'tomofish'},
     {path: '/pin_pong', component: pin_pong, name: 'pin_pong'},
-    {path: '/football', component: football, name: 'football'}
+    {path: '/football', component: football, name: 'football'},
+    {path: '/game', component: CatchGame, name: 'game'}
+
 ]
 
 export const router = createRouter({
